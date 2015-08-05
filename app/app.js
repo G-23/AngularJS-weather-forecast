c435dc4ca55a1151617eb2494c1831230be5caa9
Simone De Luca <simdeluca92@gmail.com>
2015-08-05 13:53:10 +0100

    Tidy up

--- app/app.js
+++ app/app.js
@@ -1,5 +1,4 @@
 (function(){
-	
 	var weatherApp = angular.module('weatherApp', []);
 
 	weatherApp.service('Weather', function($http) {
@@ -10,12 +9,11 @@
 
 	weatherApp.controller('weatherController', function($scope, $http, Weather) {
 		$scope.weatherForecast = {};
-		$scope.date;
+
 		$scope.getWeather = function() {
 			if($scope.city) {
 				Weather.requestWeather($scope.city).then(function(result) {
 					$scope.weatherForecast = result.data;
-					$scope.date = new Date();
 				});
 			}
 		};
